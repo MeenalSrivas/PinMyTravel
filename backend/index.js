e40c8765a4854a85388ed283adc2ab83f7246487
meenal <meenalsrivastav2004@gamil.com>
2025-03-12 23:22:12 +0530

    all origin allowed

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(express.static('./frontend/build'));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-     // Allow requests from this origin
+    origin: "*",
+     // Allow requests from all origin
     credentials: true, // Allow cookies and credentials
   })
 );
